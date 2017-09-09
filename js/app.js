0ba495170465f9ae0d851db9602d219563b49678
hyjcatty <hyjcatty@gmail.com>
2017-09-09 09:36:32 +0800

    remove the onresize event

--- js/app.js
+++ js/app.js
@@ -1402,7 +1402,7 @@ function calculate_row(){
     //console.log("Now table row is:"+table_row);
 
 }
-window.onresize=calculate_row;
+//window.onresize=calculate_row;
 function show_table_tags(){
 	$('#Warning_'+alarm_type_list[0].id+'_day').css('display','block');
 }
